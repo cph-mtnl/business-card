c18dbc8e1ecf0a92ca84529df4df2a388bae9520
cph-mtnl <mtnl@cphbusiness.dk>
2022-02-11 12:23:04 +0100

    Business card

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+            <i className="fab fa-twitter-square footer--icon"></i>
+            <i className="fab fa-facebook-square footer--icon"></i>
+            <i className="fab fa-instagram-square footer--icon"></i>
+            <i className="fab fa-linkedin footer--icon"></i>
+            <i className="fab fa-github-square footer--icon"></i>
+        </div>
+    );
+}
+
+export default Footer;
